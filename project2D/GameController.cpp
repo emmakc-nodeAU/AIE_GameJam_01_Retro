0d8f544a48da84f582e1e929d00bfc6cccf64c79
Emma Cameron <emmakc@gmail.com>
2017-02-09 12:35:57 +1100

    Commented GameController note - EC

--- project2D/GameController.cpp
+++ project2D/GameController.cpp
@@ -20,7 +20,7 @@ void GameController::Update(float deltaTime)
 	// SPAWN GENERATOR: Asteroids
 	for (int i = 0; i < iAsteroidNumber; ++i)
 	{
-		// Increasing Asteroid Numbers
+		//	Generates Asteroid Numbers
 	}
 }
 
