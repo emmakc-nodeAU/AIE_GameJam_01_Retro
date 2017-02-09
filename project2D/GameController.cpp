a5c31e84869c02c026110e0d3d637d31bdae3968
Trent Aaron Ohrwalder <trent.ohh@gmail.com>
2017-02-09 14:44:13 +1100

    Pushing up protected files, included temp code to be used later. Making functional areas and easier implimentation - TO

--- project2D/GameController.cpp
+++ project2D/GameController.cpp
@@ -1,9 +1,13 @@
 #include "GameController.h"
+#include "Application2D.h"
+#include "Input.h"
 #include "Texture.h"
+
 
 // Foward Declaring
 
-GameController::GameController() 
+/////////
+GameController::GameController()
 
 {
 
@@ -13,18 +17,96 @@ GameController::~GameController()
 {
 
 }
+/////////
 
-void GameController::Update(float deltaTime)
+bool GameController::startup()
 {
+	//m_2dRender =	new aie::Renderer2D();
+
+	//m_texture =	new aie::Texture("./textures/numbered_grid.tga");
+
+	//m_font =		new aie::Font("./font/consolas.ttf", 32);
+
+	//m_audio =		new aie::Audio("./audio/powerup.wav");
+}
+
+void GameController::shutdown()
+{
+	delete m_audio;
+	delete m_font;
+	delete m_texture;
+	delete m_2dRender;
+}
+
+
+void GameController::update(float deltaTime)
+{
+	m_timer += deltaTime;
 
 	// SPAWN GENERATOR: Asteroids
 	for (int i = 0; i < iAsteroidNumber; ++i)
 	{
+
+		aie::Input* input = aie::Input::getInstance();
+
+		if (input->wasKeyPressed(aie::INPUT_KEY_SPACE))
+		{
+			// Shoot
+		}
+
+		if (input->wasKeyPressed(aie::INPUT_KEY_A))
+		{
+			// Rotate CounterClockwise
+		}
+
+		if (input->wasKeyPressed(aie::INPUT_KEY_D))
+		{
+			// Rotate Clockwise
+		}
+
+
+		/*
+		int m_asteroid[i]; // temp until class is made.
+		int m_ship;		   // temp until class is made.
+
 		//	Generates Asteroid Numbers
+		for ((m_asteroid[i]->Position() - m_ship->Position()).glm::length() < 20)
+		{
+			// Calculates the position of the asteroid if it enters, remove health /detroy ship.
+		}
+		*/
+
+
+		/*
+		for ((m_bullet[e]->Position() - m_asteroid[i]->Position()).gml::length() < 20)
+		{
+			if (m_asteroid[i] == 1) // Large Asteroid
+			{
+				// Destroy large asteroid, spawn 2 medium asteroids and add points.
+			}
+
+			if (m_asteroid[i] == 2) // Medium Asteroid
+			{
+				// Destroy medium asteroid, spawn 1 small asteroid and add points.
+			}
+
+			else
+			{
+				// Destroy Asteroid and add points.
+			}
+		}
+		*/
+
 	}
+
+
+
+
 }
 
-void GameController::Render2D()
+void GameController::draw()
 {
+	clearScreen();
 
+	m_2dRender->begin();
 }
--- project2D/GameController.h
+++ project2D/GameController.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "Application2D.h"
+#include <glm/ext.hpp>
+#include <glm/glm.hpp>
+#include "Renderer2D.h"
 //#include "BaseObject.h"
 //#include "Score.h"
 //#include "Canvas.h"
@@ -10,7 +13,7 @@
 //	class Texture;
 //}
 
-class GameController
+class GameController : public aie::Application
 {
 public:
 	GameController();
@@ -25,15 +28,30 @@ public:
 	//const aie::Texture* () {};
 
 	// Update
-	void Update(float deltaTime);
+	void update(float deltaTime) override;
 
 	// Render2D
-	virtual void Render2D();
+	virtual void draw() override;
+
+	virtual bool startup() override;  // Use this to load textures
+	virtual void shutdown() override; // Use this to delete textures
+	/*
+	Startup is an easier way to preload assets in for them to be used later
+	in other classfiles rather than loading them in the constructor which might
+	break the application / leak memory.
+
+	Shutdown will be used similiar but this will replace the deconstructor.
+	*/
 
 
+protected:
 
+	aie::Renderer2D*	m_2dRender;      // Renders the gameObject.
+	aie::Texture*		m_texture;       // Gets the allocated texture.
+	aie::Font*			m_font;          // Loads the font.
+	aie::Audio*			m_audio;         // Imports Audio.
 
-private:
+	float m_timer;
 
 
 };
