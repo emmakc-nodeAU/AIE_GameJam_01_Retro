c78fb5d5f5dcee22c7fa114df03e41a94992da71
Emma Cameron <emmakc@gmail.com>
2017-02-09 15:49:52 +1100

    BaseObject update() - EC & TO

---
+++ project2D/BaseObject.cpp
@@ -0,0 +1,47 @@
+#include "GameController.h"
+#include "Application2D.h"
+#include "Input.h"
+#include "Texture.h"
+#include "BaseObject.h"
+
+BaseObject::BaseObject(float a_floatX, float a_floatY)
+{
+	m_fX = a_floatX;
+	m_fY = a_floatY;
+	m_fmovementX = 0;
+	m_fmovementY = 0;
+}
+
+BaseObject::~BaseObject()
+{
+
+}
+
+void BaseObject::update(float deltaTime)
+{
+	// Update Movement
+	m_fX += m_fmovementX;
+	m_fY += m_fmovementY;
+
+	// Game Time
+	m_timer += deltaTime;
+
+	// INPUT for Ship movement
+	aie::Input* input = aie::Input::getInstance();
+
+	if (input->wasKeyPressed(aie::INPUT_KEY_A))
+	{
+		// Rotate CounterClockwise
+	}
+
+	if (input->wasKeyPressed(aie::INPUT_KEY_D))
+	{
+		// Rotate Clockwise
+	}
+
+	// BULLET MOVEMENT
+	if (input->wasKeyPressed(aie::INPUT_KEY_SPACE))
+	{
+		// Shoot
+	}
+}
--- project2D/BaseObject.h
+++ project2D/BaseObject.h
@@ -1,1 +1,44 @@
 #pragma once
+#include "GameController.h"
+#include "Application2D.h"
+#include <glm/ext.hpp>
+#include <glm/glm.hpp>
+#include "Renderer2D.h"
+
+class BaseObject : public aie::Application
+{
+public:
+	BaseObject(float a_floatX, float a_floatY);
+
+	~BaseObject();
+
+	// VARIABLES
+
+	int iShip;
+	int iAsteroid;
+	int iBullet;
+
+	// FUNCTIONS
+	// 1. Movement
+	// 2. Position
+	// 3. Update
+
+
+	// 1. MOVEMENT
+	
+	// 2. POSITION
+
+	// 3. UPDATE
+	void update(float deltaTime) override;
+
+protected:
+
+	// Position
+	float m_fX;
+	float m_fY;
+	float m_timer;
+	// Movement
+	float m_fmovementX;
+	float m_fmovementY;
+
+};
